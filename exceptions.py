390bf56a5970f6d86cafc15679361372aec8b8b5
jmssjess <woo0008@jmss.vic.edu.au>
2016-02-08 14:59:08 +1100

    edits to exception.py

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,17 @@
-my_list = [1,2,3,5]
+my_list = [0,1,2,3,4,5]
+x = int(input('State an number under 5. '))
+try:
+ x = my_list[x]
+ print(x)
+except IndexError:
+ print("Out of Range")
 
-#try:
-x = my_list[5]
-#except IndexError:
-#    print("Out of Range")
+t = True
+while t:
+ try:
+  y = input ('Write a number for me. ')
+  y = float(y)
+  t = False
+ except ValueError:
+  print('I need a numberal please, not ',y)
+print(y)
